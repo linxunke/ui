272e28b574f4612d098323633229574029b8ca8e
linxunke <250023827@qq.com>
2019-03-05 16:59:26 +0800

    提交

--- src/main/java/com/ztzh/ui/controller/TestController.java
+++ src/main/java/com/ztzh/ui/controller/TestController.java
@@ -1,22 +1,20 @@
 package com.ztzh.ui.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ztzh.ui.service.TestService;
 
-@Controller
+@RestController
 public class TestController {
 	Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	@Autowired TestService testService;
 	
-	@RequestMapping(value="test")
+	@RequestMapping(value="/test")
 	public String testInsert() {
 		return "test";
 	}
