48aacef291102b4119f95fd1d30b6b71f23b0398
sylvanashan123 <2290119093@qq.com>
2019-03-15 09:03:21 +0800

    3/15解决账号不存在的问题

--- src/main/java/com/ztzh/ui/service/impl/UserServiceImpl.java
+++ src/main/java/com/ztzh/ui/service/impl/UserServiceImpl.java
@@ -16,104 +16,114 @@ import com.ztzh.ui.utils.VerifyLengthUtil;
 import com.ztzh.ui.vo.ResponseVo;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserInfoDomainMapper userInfoDomainMapper;
-	
+
 	@Override
 	@Transactional
 	public RegisterResultBo register(UserInfoDomain user) {
 		RegisterResultBo registerResBo = new RegisterResultBo();
-		int accountResult = VerifyLengthUtil.objectLengthShortThan20(user.getUserAccount());
-		int nicknameResult = VerifyLengthUtil.objectLengthShortThan20(user.getUserNickname());
-		int weiXinResult = VerifyLengthUtil.objectLengthShortThan20(user.getUserWeixin());
-		int passwordResult = VerifyLengthUtil.objectLengthForPassword(user.getUserPassword());
-		if(accountResult == UserConstants.CHECK_DATA_LENGTH_FALSE){
+		int accountResult = VerifyLengthUtil.objectLengthShortThan20(user
+				.getUserAccount());
+		int nicknameResult = VerifyLengthUtil.objectLengthShortThan20(user
+				.getUserNickname());
+		int weiXinResult = VerifyLengthUtil.objectLengthShortThan20(user
+				.getUserWeixin());
+		int passwordResult = VerifyLengthUtil.objectLengthForPassword(user
+				.getUserPassword());
+		if (accountResult == UserConstants.CHECK_DATA_LENGTH_FALSE) {
 			registerResBo.setCode(UserConstants.CHECK_DATA_LENGTH_FALSE);
 			return registerResBo;
-		}else if(nicknameResult == UserConstants.CHECK_DATA_LENGTH_FALSE){
+		} else if (nicknameResult == UserConstants.CHECK_DATA_LENGTH_FALSE) {
 			registerResBo.setCode(UserConstants.CHECK_DATA_LENGTH_FALSE);
 			return registerResBo;
-		}else if(weiXinResult == UserConstants.CHECK_DATA_LENGTH_FALSE){
+		} else if (weiXinResult == UserConstants.CHECK_DATA_LENGTH_FALSE) {
 			registerResBo.setCode(UserConstants.CHECK_DATA_LENGTH_FALSE);
 			return registerResBo;
-		}else if(passwordResult == UserConstants.CHECK_DATA_LENGTH_FALSE){
+		} else if (passwordResult == UserConstants.CHECK_DATA_LENGTH_FALSE) {
 			registerResBo.setCode(UserConstants.CHECK_DATA_LENGTH_FALSE);
 			return registerResBo;
-		}else{
+		} else {
 			user.setAuthority(new Integer(UserConstants.USER_AUTHORITY_NORMAL));
 			user.setIsValid(new Integer(UserConstants.USER_IS_VALID_YES));
 			user.setRegisterTime(GetSYSTime.systemTime());
-			//执行dao层方法
+			// 执行dao层方法
 			userInfoDomainMapper.insert(user);
-			String userId = userInfoDomainMapper.getUserIdByAccount(user.getUserAccount());
+			String userId = userInfoDomainMapper.getUserIdByAccount(user
+					.getUserAccount());
 			registerResBo.setCode(UserConstants.CHECK_DATA_LENGTH_TRUE);
 			registerResBo.setUserId(userId);
 			return registerResBo;
 		}
-		
+
 	}
+
 	@Override
 	public String encrypt(String password) {
 		String result = MD5Util.encode2hex(password);
 		return result;
 	}
-	
+
 	@Override
 	public String accountValidate(String account) {
-		
+
 		return null;
 	}
+
 	@Override
 	public String getUserIdByAccount(String account) {
-		
+
 		return userInfoDomainMapper.getUserIdByAccount(account);
 	}
+
 	@Override
 	public boolean checkUserAccountIsValue(String account) {
 		String userId = userInfoDomainMapper.getUserIdByAccount(account);
-		if(null==userId){
+		if (null == userId) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	@Override
 	public LoginResultBo login(String account, String password) {
-		UserInfoDomain userInfoDomain = userInfoDomainMapper.getUserInfoByAccount(account);
+		UserInfoDomain userInfoDomain = userInfoDomainMapper
+				.getUserInfoByAccount(account);
 		boolean isValidResult;
-		Integer isValid = userInfoDomain.getIsValid();
-		if(UserConstants.USER_IS_VALID_YES.equals(isValid.toString())){
-			isValidResult= true; //用户有效
-		} else{
-			isValidResult= false; //用户无效
-		}
 		boolean isAccountPasswordMatchResult;
-		String correctPassword = userInfoDomain.getUserPassword();
-		if(correctPassword.equals(password)){
-			isAccountPasswordMatchResult= true;  //账号密码匹配
-		}
-		else{
-			isAccountPasswordMatchResult= false;  //账号密码不匹配
-		}
 		LoginResultBo loginResultBo = new LoginResultBo();
-		if(isAccountPasswordMatchResult){
-			if(isValidResult){
-				loginResultBo.setStatus(ResponseVo.STATUS_SUCCESS);
+		if (null != userInfoDomain) {
+			Integer isValid = userInfoDomain.getIsValid();
+			if (UserConstants.USER_IS_VALID_YES.equals(isValid.toString())) {
+				isValidResult = true; // 用户有效
+			} else {
+				isValidResult = false; // 用户无效
 			}
-			else{
-				loginResultBo.setStatus(ResponseVo.STATUS_VALUE_FALSE);
+			String correctPassword = userInfoDomain.getUserPassword();
+			if (correctPassword.equals(password)) {
+				isAccountPasswordMatchResult = true; // 账号密码匹配
+			} else {
+				isAccountPasswordMatchResult = false; // 账号密码不匹配
 			}
-		}
-		else{
+			if (isAccountPasswordMatchResult) {
+				if (isValidResult) {
+					loginResultBo.setStatus(ResponseVo.STATUS_SUCCESS);
+				} else {
+					loginResultBo.setStatus(ResponseVo.STATUS_VALUE_FALSE);
+				}
+			} else {
+				loginResultBo.setStatus(ResponseVo.STATUS_ACCOUNT_OR_PASSWORD_FALSE);
+			}
+			loginResultBo.setId(userInfoDomain.getId().toString());
+			loginResultBo.setUserPhotoUrl(userInfoDomain.getUserPhotoUrl());
+		} else{
 			loginResultBo.setStatus(ResponseVo.STATUS_ACCOUNT_OR_PASSWORD_FALSE);
 		}
-		loginResultBo.setId(userInfoDomain.getId().toString());
-		loginResultBo.setUserPhotoUrl(userInfoDomain.getUserPhotoUrl());
 		return loginResultBo;
 	}
-	
+
 }
